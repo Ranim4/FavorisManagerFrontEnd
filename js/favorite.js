a8c8266ec44471264d0b8bd4ad1c043a2b6cd181
Ranim <ranim.mouhamad@hotmail.com>
2023-09-04 15:21:33 +0200

    Features have been added

--- js/favorite.js
+++ js/favorite.js
@@ -1,25 +1,29 @@
 angular.module('favoriteApp', []) // favoriteApp lie au Html
-  .controller('FavoritesController', function ($scope, $http) {
+  .controller('FavoritesController', function ($scope, $http, $timeout) {
 
     $scope.categories = [];
     $scope.realCategories = [];
     $scope.favorites = [];
 
+    $scope.isInvalidLink = false;
     $scope.selectAll = false;
     //selected criterion for filtering favorites list
     $scope.categoryFilter = 0;
     $scope.selectedCategory = 0;
-    
+
+    $scope.showToast = false;
+    $scope.toastMessage = '';
+
     //ids list of favorite want to delete
     $scope.idsToDelete = [];
     //selected criterion for sorting favorites list
     $scope.selectedSortCriteria = 'all';
     //sorting options
     $scope.sortCriteriaOptions = [
-      {label: 'Sort by ...', value: 'all' },
+      { label: 'Sort by ...', value: 'all' },
       { label: 'Sort by Category', value: 'category' },
       { label: 'Sort by Date', value: 'date' }
-  ];
+    ];
 
     $scope.mode = 'view';
 
@@ -27,8 +31,12 @@ angular.module('favoriteApp', []) // favoriteApp lie au Html
 
     $scope.setMode = function (text) {
       if (text === 'creation') {
+
         $scope.realCategories = $scope.categories.filter(function (c) { return c.id !== 0 });
+        console.log($scope.realCategories);
+        //$scope.realCategories.unshift({id:0, label:'Choose one ...', selected: true});
         var idx = $scope.realCategories.map(function (c) { return c.id }).indexOf($scope.categoryFilter);
+
         if (idx < 0) idx = 0;
 
         $scope.favorite = {
@@ -42,17 +50,43 @@ angular.module('favoriteApp', []) // favoriteApp lie au Html
     $scope.cancel = function () {
       $scope.setMode('view');
     }
+
+    $scope.verifyLink = function (link) {
+
+    }
+
     const root = "http://localhost:8080";
 
-    $scope.validate = function () {
-      $http.post(root + '/api/favorites/' + $scope.favorite.category + '/favorite', { id: null, link: $scope.favorite.link }).then(
-        function () {
-          $scope.refresh();
-          $scope.setMode('view');
-        }, function (error) {
-          alert(error.data.message);
-        }
-      )
+    $scope.validate = function (id, link) {
+
+      // Check if the link is valid
+      //if it's starts with "http://" or "https://" => isInvalidLink = false
+      var isValidLink = /^https?:\/\//.test(link);
+
+      $scope.isInvalidLink = !isValidLink;
+      if (!isValidLink) {
+        // isValidLink.test(link);
+        alert('Invalid link. Please enter a valid link.');
+        $scope.isInvalidLink = true;
+      }
+      else {
+        $scope.isInvalidLink = false
+        console.log(id);
+        $http.post(root + '/api/favorites/' + id + '/favorite',
+          { id: null, link: $scope.favorite.link }).then(
+            function () {
+              $scope.refresh();
+              $scope.setMode('view');
+            }, function (error) {
+              alert(error.data.message);
+            }
+          )
+        // Show toast & Hide it after a 3 seconds
+        Toastify({
+          text: "Favorite added successfully!", 
+          duration: 3000  
+          }).showToast(); 
+      }
     }
 
     $scope.refresh = function () {
@@ -113,7 +147,7 @@ angular.module('favoriteApp', []) // favoriteApp lie au Html
 
     //filter the favorites list according to selected category
     $scope.filterFavorites = function (categoryId) {
-      if (categoryId !=0){
+      if (categoryId != 0) {
         const url = root + '/api/favorites/' + $scope.categories[categoryId].label;
         $http.get(url).then(function (response) {
           console.log(categoryId);
@@ -129,11 +163,12 @@ angular.module('favoriteApp', []) // favoriteApp lie au Html
 
     $scope.deleteSelected = function () {
       console.log($scope.idsToDelete);
-      
+
       $http.delete(root + '/api/favorites/' + $scope.idsToDelete)
         .then(function () {
           console.log('Selected favorites deleted successfully');
           $scope.refresh();
+          $scope.idsToDelete = [];
         }).catch(function (error) {
           console.error('Error during removing the selected favorites', error);
         });
@@ -148,13 +183,16 @@ angular.module('favoriteApp', []) // favoriteApp lie au Html
         }).catch(function (error) {
           console.error('Error when sorting by category', error);
         });
-      }else if (selectedSortCriteria === 'date') {
+      } else if (selectedSortCriteria === 'date') {
         const url = root + '/api/favorites/sortedByDate';
         $http.get(url).then(function (response) {
           $scope.favorites = response.data;
         }).catch(function (error) {
           console.error('Error when sorting by date', error);
         });
+      }
+      else {
+        $scope.refresh();
       }
     }
 
